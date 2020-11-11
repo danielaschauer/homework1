d99c2402e371f2a91962d412f28715ae9802a7fc
Daniel Aschauer <dani.aschauer@gmail.com>
2020-11-11 12:29:24 +0100

    add test function
    Adds a test function to the repository.

---
+++ test_fraction_addition.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 23:10:17 2020
+
+@author: Daniel
+"""
+
+from homework1 import is_prime
+from homework1 import add_frac
+
+def test_is_prime():
+    assert is_prime(11) == True
+ 
+def test_add_frac():
+    assert add_frac(12,34,24,65) == print("Ergebnis ungekürzt: ",12,"/",34,"+",24,"/",65,"=",1596,"/",2210)
+
